df9a02d70182704969d41b92f214ce5ea4be0a52
Gabriel Vieira <gabrielvieira840@gmail.com>
2022-03-30 22:00:32 -0300

    season 1.1
    OOP v 1.0

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Gabriel"
+	var numeroAgencia int = 001
+	var numeroConta int = 987654
+	var saldo float64 = 1000.40
+
+	fmt.Println("Olá, ", titular)
+	fmt.Println("Agência: ", numeroAgencia)
+	fmt.Println("Conta: ", numeroConta)
+	fmt.Println("Saldo: ", saldo)
+}
