97ee2b93b06a664a4fd6a9de4f6b512aded1b8a2
Gabriel Vieira <gabrielvieira840@gmail.com>
2022-03-31 09:25:21 -0300

    testando struct

--- main.go
+++ main.go
@@ -10,5 +10,9 @@ type ContaCorrente struct {
 }
 
 func main() {
-	fmt.Println(ContaCorrente{"Gabriel", 123, 456, 1000.00})
+	contaGabriel := ContaCorrente{"Gabriel", 123, 456, 1000.00}
+	contaTeste := ContaCorrente{titular: "Teste", saldo: 150.0}
+
+	fmt.Println(contaGabriel)
+	fmt.Println(contaTeste)
 }
