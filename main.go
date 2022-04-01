5d954969a6974ac23eff72a55f965fc041aa1af8
Gabriel Vieira <gabrielvieira840@gmail.com>
2022-03-31 21:45:36 -0300

    season 3
    adiciona multiplos retornos para função

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ type ContaCorrente struct {
 func main() {
 	contaGabriel := ContaCorrente{"Gabriel", 123, 456, 1000.00}
 
-	fmt.Println(contaGabriel.Sacar(800.00))
+	//fmt.Println(contaGabriel.Sacar(800.00))
+	fmt.Println(contaGabriel.saldo)
+	status, valor := contaGabriel.Depositar(500.00)
+	fmt.Println(status, valor)
 	fmt.Println(contaGabriel.saldo)
 }
 
@@ -23,5 +26,14 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 		return "Saque realizado com sucesso"
 	} else {
 		return "saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(depositar float64) (string, float64) {
+	if depositar > 0 {
+		c.saldo += depositar
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Deposito não realizado", c.saldo
 	}
 }
